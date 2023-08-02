103ad3c46701add3080c3709dc5dd421828bee8d
John Kervin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-08-02 09:45:12 +0200

    creando el proyecto

---
+++ src/main/java/com/calendario/makehabits/MakehabitsApplication.java
@@ -0,0 +1,13 @@
+package com.calendario.makehabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakehabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakehabitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calendario/makehabits/MakehabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendario.makehabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakehabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

45f571ffc3fc8f86378efaf6ca08f9e83f072d48
Yasmine Bahaji <yasmine.bahaji98@gmail.com>
2023-08-03 12:26:47 +0200

    frontend add bootstrap

--- makehabits-react/src/main.tsx
+++ makehabits-react/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import UserRegistration from "./components/UserRegistration";
 import "./index.css";
+import  "./pages/Landin"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

82573e39fc2e7b9a445bdf9eeb7358dfc6dd267a
Yasmine Bahaji <yasmine.bahaji98@gmail.com>
2023-08-03 13:28:46 +0200

    diseño

--- makehabits-react/src/main.tsx
+++ makehabits-react/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import UserRegistration from "./components/UserRegistration";
-import "./index.css";
-import  "./pages/Landin"
+import "bootstrap/dist/css/bootstrap.css";
+import  Landing from "./pages/Landing";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserRegistration />
+    <Landing />
   </React.StrictMode>,
 );
---
+++ makehabits-react/src/pages/Landing.tsx
@@ -0,0 +1,27 @@
+const Landing = () => {
+  
+    return (
+      
+    <div className="container-fluid text-center mb-1 mt-1 text-light">
+        <div className="row ">
+            <div style={{backgroundColor: "pink"}} className="col-12 border border-white ">
+                -
+            </div>
+            </div>
+        <div className="row mb-1 mx-1 vh-100">
+            <div style={{backgroundColor: "info"}} className="col-2 border border-white ">
+                -
+            </div>
+            <div style={{backgroundColor: "info"}} className="col-7 border border-white">
+                -
+            </div>
+            <div style={{backgroundColor: "info"}} className="col-3 border border-white ">
+                -
+            </div>
+        </div>
+        </div>
+      
+    );
+  };
+  
+  export default Landing;

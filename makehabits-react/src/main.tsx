68f91ccd5e0dfb4ae6778b4e050c86e91a307598
Victor Jamet <vjamet15@gmail.com>
2023-08-07 13:03:29 +0200

    Landing sidebar

--- makehabits-react/src/main.tsx
+++ makehabits-react/src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import UserRegistration from "./components/UserRegistration";
+import "./App.css"
 import "bootstrap/dist/css/bootstrap.css";
 import  Landing from "./pages/Landing";
+import './assets/fonts/Publio W01 Regular/Publio W01 Regular.ttf';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Landing />
+    <Landing/>
   </React.StrictMode>,
 );

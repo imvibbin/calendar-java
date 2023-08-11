faeccd3f9fa75dbff66f30f2bae01c7bd53ac5b9
John Kervin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-08-11 13:53:54 +0200

    registro redirecciona a landing cuando el proceso ha sido correcto

--- makehabits-react/src/pages/RegisterPage.tsx
+++ makehabits-react/src/pages/RegisterPage.tsx
@@ -1,7 +1,8 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import UserInterface from "../models/UserInterface";
 import { createUser } from "../services/UserService";
-import { Link } from "react-router-dom";
+import CustomError from "../models/CustomError";
+import { useNavigate } from "react-router-dom";
 
 import "./styles/RegisterPage.css";
 
@@ -15,19 +16,32 @@ const RegisterPage = () => {
 
   const [user, setUser] = useState<UserInterface>(userInitial);
   const [btnStatus, setBtnClicked] = useState(false);
-  const [registrationSuccess, setRegistrationSuccess] = useState(false);
+  const navigate = useNavigate();
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setUser({ ...user, [name]: value });
   };
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    setBtnClicked(!btnStatus);
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    setBtnClicked(true);
     event.preventDefault();
     console.log(`Button status: ${btnStatus}`);
     console.log(user);
-    createUser(user);
+    try {
+      const response = await createUser(user);
+      console.log("User created:", response);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    } catch (error: any) {
+      const backendError = error as CustomError; // Cast to custom error type
+      if (backendError.message) {
+        console.error("Failed to create user:", backendError.message);
+      } else {
+        console.error("Failed to create user: An unknown error occurred.");
+      }
+    }
   };
 
   return (
@@ -35,8 +49,13 @@ const RegisterPage = () => {
       <div className="main-container container-fluid vh-100">
         <div className="row h-auto vh-100 overflow-hidden">
           <div className="main-container-banner col-md-7 d-flex align-items-center justify-content-center overflow-hidden">
-            <div className="circle1 rounded-circle"></div>
-            <div className="circle2 rounded-circle"></div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path
+                fill="#0099ff"
+                fillOpacity="1"
+                d="M0,224L30,213.3C60,203,120,181,180,192C240,203,300,245,360,240C420,235,480,181,540,138.7C600,96,660,64,720,69.3C780,75,840,117,900,149.3C960,181,1020,203,1080,192C1140,181,1200,139,1260,117.3C1320,96,1380,96,1410,96L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
+              ></path>
+            </svg>
           </div>
           <div className="main-container-content col-5 p-5 position-relative">
             <div className="main-container-content-form position-absolute">
--- makehabits-react/src/services/UserService.ts
+++ makehabits-react/src/services/UserService.ts
@@ -4,69 +4,62 @@ const BASE_URL = "http://localhost:8080/users";
 
 // Function to make a GET request to fetch all users
 async function getAllUsers(): Promise<UserInterface[]> {
-  try {
-    const response = await fetch(BASE_URL);
-    if (!response.ok) {
-      throw new Error("Network response was not ok.");
-    }
-    return await response.json();
-  } catch (error: any) {
-    throw new Error("Failed to fetch users: " + error.message);
+  const response = await fetch(BASE_URL);
+
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    throw new Error(errorResponse.message); // Throw an error with the error message from the backend
   }
+
+  return response.json();
 }
 
 // Function to make a POST request to create a new user
 async function createUser(newUser: UserInterface): Promise<UserInterface> {
-  try {
-    console.log(`${BASE_URL}/registration`);
-    const response = await fetch(`${BASE_URL}/registration`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newUser),
-    });
-    if (!response.ok) {
-      throw new Error("Network response was not ok.");
-    }
-    console.log(JSON.stringify(response));
-    return await response.json();
-  } catch (error: any) {
-    throw new Error("Failed to create user: " + error.message);
+  const response = await fetch(`${BASE_URL}/registration`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    throw new Error(errorResponse.message); // Throw an error with the error message from the backend
   }
+
+  return response.json();
 }
 
 // Function to make a PUT request to update a user
 async function updateUser(updatedUser: UserInterface): Promise<UserInterface> {
-  try {
-    const response = await fetch(`${BASE_URL}/${updatedUser.user_id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedUser),
-    });
-    if (!response.ok) {
-      throw new Error("Network response was not ok.");
-    }
-    return await response.json();
-  } catch (error: any) {
-    throw new Error("Failed to update user: " + error.message);
+  const response = await fetch(`${BASE_URL}/${updatedUser.user_id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedUser),
+  });
+
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    throw new Error(errorResponse.message); // Throw an error with the error message from the backend
   }
+  return await response.json();
 }
 
 // Function to make a DELETE request to delete a user
 async function deleteUser(userId: number): Promise<void> {
-  try {
-    const response = await fetch(`${BASE_URL}/${userId}`, {
-      method: "DELETE",
-    });
-    if (!response.ok) {
-      throw new Error("Network response was not ok.");
-    }
-  } catch (error: any) {
-    throw new Error("Failed to delete user: " + error.message);
+  const response = await fetch(`${BASE_URL}/${userId}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    throw new Error(errorResponse.message); // Throw an error with the error message from the backend
   }
+  return await response.json();
 }
 
 export { getAllUsers, createUser, updateUser, deleteUser };

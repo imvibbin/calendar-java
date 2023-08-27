ff4f03b6ac1c89f4dcdc55a9c203fc3dcc5bf133
John Kevin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-08-27 22:12:03 +0200

    he añadido un grid al calendario

---
+++ makehabits-react/src/components/WeeklyView/ViewGrid.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface GridProps {
+  rows: number;
+  columns: number;
+}
+
+const WeeklyViewGrid: React.FC<GridProps> = ({ rows, columns }) => {
+  const grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < columns; j++) {
+      row.push(<td key={j}>Cell {i * columns + j + 1}</td>);
+    }
+    grid.push(<tr key={i}>{row}</tr>);
+  }
+
+  return (
+    <table>
+      <tbody>{grid}</tbody>
+    </table>
+  );
+};
+
+export default WeeklyViewGrid;
--- makehabits-react/src/components/WeeklyView/WeeklyView.tsx
+++ makehabits-react/src/components/WeeklyView/WeeklyView.tsx
@@ -1,30 +1,44 @@
-/* import React from 'react'; */
-import './WeeklyView.scss'; // Make sure to create this CSS file for styling
-import TimeCells from '../Lib/DraggableLib/TimeCells'
+import React from "react";
+import "./WeeklyView.scss"; // Make sure to create this CSS file for styling
+import TimeCells from "../Lib/DraggableLib/TimeCells";
+import ViewGrid from "./ViewGrid/ViewGrid";
 
 const WeeklyView = () => {
   const numRows = 16;
   const numCols = 8;
-  const daysOfWeek = ['', 'Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  /* const daysOfMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'];*/
-  const hourRange = ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM','12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM','4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM','8:00 PM', '9:00 PM', '10:00 PM'];
-  
+  const daysOfWeek = ["", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const hourRange = [
+    "8:00 AM",
+    "9:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12:00 PM",
+    "1:00 PM",
+    "2:00 PM",
+    "3:00 PM",
+    "4:00 PM",
+    "5:00 PM",
+    "6:00 PM",
+    "7:00 PM",
+    "8:00 PM",
+    "9:00 PM",
+    "10:00 PM",
+  ];
 
-  const generateDayCells = () => { 
+  const generateDayCells = () => {
     const DayCells = [];
     for (let col = 0; col < numCols; col++) {
-
       const cellKey = `cell--${0}-${col}`;
       const cellContent = daysOfWeek[col];
       const cellDayClass = "grid--day--cell";
       const cellStyle = {
-        backgroundColor: 'white'
+        backgroundColor: "white",
       };
-      
+
       DayCells.push(
         <div key={cellKey} className={cellDayClass} style={cellStyle}>
           {cellContent}
-        </div>
+        </div>,
       );
     }
     return DayCells;
@@ -37,47 +51,29 @@ const WeeklyView = () => {
       const cellContent = hourRange[row];
       const cellHourClass = "grid--hour--cell";
       const cellStyle = {
-        backgroundColor: 'white'
+        backgroundColor: "white",
       };
-      
+
       HourCells.push(
         <div key={cellKey} className={cellHourClass} style={cellStyle}>
           {cellContent}
-        </div>
+        </div>,
       );
     }
     return HourCells;
   };
 
-
-
-
   return (
     <div className="CalendarUI">
       <div className="dayHeader">{generateDayCells()}</div>
 
-      <div className="grid">
-        <div className="grid--hour">
-        {generateTimeCells()}
+      <div className="grid border">
+        <div className="grid--hour">{generateTimeCells()}</div>
+        <div className="grid--calendar position-relative">
+          <ViewGrid rows={15} columns={7} />
+          <TimeCells />
         </div>
-        <div className="grid--calendar">
-{/*         <div className="under-mask">
-      {Array.from({ length: 105 }, (_, index) => (
-        <div key={index} className="under-mask--cell">
-            
-        </div>
-      ))}
-    </div> */}
-    <TimeCells/>
-          {/* <div className='calendar--row'>1</div>
-          <div className='calendar--row'>2</div>
-          <div className='calendar--row'>3</div>
-          <div className='calendar--row'>4</div>
-          <div className='calendar--row'>5</div>
-          <div className='calendar--row'>6</div>
-          <div className='calendar--row'>7</div> */}
-        </div>
-        </div>
+      </div>
     </div>
   );
 };

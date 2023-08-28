47e41ad0e893eae93d3a25bf1d0411a50e896061
John Kervin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-08-28 14:01:32 +0200

    he solucionado unos problemas

--- makehabits-react/src/components/Elements/NavBar/NavBar.tsx
+++ makehabits-react/src/components/Elements/NavBar/NavBar.tsx
@@ -1,25 +1,12 @@
-import React from "react";
 import perfil from "../../../assets/images/FrontPage/NavBar/perfil.png";
 import notificacion from "../../../assets/images/FrontPage/NavBar/notificacion.png";
 import back from "../../../assets/images/FrontPage/NavBar/back.png";
 import next from "../../../assets/images/FrontPage/NavBar/next.png";
 import DropdownUserLib from "../../Lib/AntLib/DropdownUserLib/DropdownUserLib";
-import UserInterface from "../../../models/UserInterface";
 
 import "./NavBar.scss";
 
-const NavBar: React.FC<UserInterface> = () => {
-  /* const navigate = useNavigate(); */
-  const USER_DATA = localStorage.getItem("USER_DATA");
-  /*   console.log(USER_DATA); */
-
-  const handleLogout = () => {
-    localStorage.removeItem("USER_DATA");
-    setTimeout(() => {
-      window.location.reload();
-    }, 1000);
-  };
-
+const NavBar = () => {
   return (
     <header className="header">
       <div className="header--weekview">
@@ -162,18 +149,8 @@ const NavBar: React.FC<UserInterface> = () => {
         >
           <path d="M160-200v-60h80v-304q0-84 49.5-150.5T420-798v-22q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v22q81 17 130.5 83.5T720-564v304h80v60H160Zm320-302Zm0 422q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM300-260h360v-304q0-75-52.5-127.5T480-744q-75 0-127.5 52.5T300-564v304Z" />
         </svg>
-
         <DropdownUserLib />
       </div>
-
-      {/* <button onClick={() => navigate("/login")}>Login</button>
-        <button onClick={() => navigate("/registration")}>Register</button>
-        {USER_DATA != null && (
-          <>
-            <h1>Welcome, logged in user!</h1>
-            <button onClick={handleLogout}>Logout</button>
-          </>
-        )} */}
     </header>
   );
 };

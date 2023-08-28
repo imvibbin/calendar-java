1f585b970aabba35c067f4b426757ce7eeb9457a
John Kervin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-08-28 13:53:16 +0200

    logout funcional

--- makehabits-react/src/components/Lib/AntLib/PopUpLib/PopUpLib.tsx
+++ makehabits-react/src/components/Lib/AntLib/PopUpLib/PopUpLib.tsx
@@ -1,56 +1,64 @@
-import React, { useState } from 'react';
-import { Button, Modal, ConfigProvider, DatePicker, Input, Form, TimePicker } from 'antd';
-import  EventInterface  from '../../../../models/EventInterface'; // Import the EventInterface
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ConfigProvider,
+  DatePicker,
+  Input,
+  Form,
+  TimePicker,
+} from "antd";
+import EventInterface from "../../../../models/EventInterface"; // Import the EventInterface
 
-import './PopUpLib.scss';
+import "./PopUpLib.scss";
 
 /* import TimePickerLib from '../TimePickerLib/TimePickerLib'; */
 const PopUpLib: React.FC = () => {
   const { RangePicker } = DatePicker;
   const [form] = Form.useForm();
-  const format = 'HH:mm';
+  const format = "HH:mm";
   const defaultEventData: EventInterface = {
-    id: 1,
-    name: '',
-    description: '',
-    hourrange: [],
+    task_id: 1,
+    user_id: 1,
+    task_name: "",
+    task_description: "",
+    task_hourrange: [],
   };
-const [selectedInput, setSelectedInput] = useState<string>('appointment');
-const [eventData, setEventData] = useState<EventInterface>(defaultEventData);
-const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedInput, setSelectedInput] = useState<string>("appointment");
+  const [eventData, setEventData] = useState<EventInterface>(defaultEventData);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedInput(e.target.value);
 
-const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setSelectedInput(e.target.value);
+    const newType = e.target.value as Form["type"];
+    const defaultEventData: EventInterface = {
+      task_id: 1,
+      task_name: "",
+      task_description: "",
+      task_hourrange: [],
+      task_type: newType,
+      task_daterange: newType === "appointment" ? [] : [],
+      task_habitRepeated: newType === "habit" ? 0 : 0,
+    };
 
-  const newType = e.target.value as Form['type'];
-  const defaultEventData: EventInterface = {
-    id: 1,
-    name: '',
-    description: '',
-    hourrange: [],
-    type: newType,
-    daterange: newType === 'appointment' ? [] : [],
-    habitRepeated: newType === 'habit' ? 0 : 0,
+    setEventData(defaultEventData);
   };
 
-  setEventData(defaultEventData);
-};
-
-const onFinish = (values: any) => {
+  const onFinish = (values: any) => {
     const jsonEventData: EventInterface = {
-    ...eventData,
-    ...values,
+      ...eventData,
+      ...values,
+    };
+
+    localStorage.setItem("eventData", JSON.stringify(jsonEventData));
+    console.log(jsonEventData);
+    setIsModalOpen(false);
+    form.resetFields();
   };
 
-  localStorage.setItem('eventData', JSON.stringify(jsonEventData));
-  console.log(jsonEventData);
-  setIsModalOpen(false);
-  form.resetFields();
-};
-  
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
 
   const showModal = () => {
@@ -66,130 +74,139 @@ const onFinish = (values: any) => {
     form.resetFields();
   };
   const TotaDisableHours = (start: number, end: number) => {
-    const TotaDisableHours = [start,end];
-   /*  const Events = localStorage.getItem(); */
-    
+    const TotaDisableHours = [start, end];
+    /*  const Events = localStorage.getItem(); */
+
     for (let i = start; i < end; i++) {
-        TotaDisableHours.push(i);
+      TotaDisableHours.push(i);
     }
     return TotaDisableHours;
   };
 
-const disabledDateTime = () => ({
+  const disabledDateTime = () => ({
     disabledHours: () => TotaDisableHours(0, 9),
-
   });
-
-
 
   return (
     <>
-    <ConfigProvider
-    theme={{
-      components: {
-        Button: {
-          colorPrimary: '#f564a9',
-          colorBorder: '#f564a9',
-          colorPrimaryActive:'#f564a9',
-          colorText: '#533b4d',
-          colorLinkActive:'#fae3c6',
-          colorPrimaryHover:'#f564a9',
-        },
-        Modal:{
-          colorBgMask:'rgba(245, 100, 169, 0.15)'
-        }
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              colorPrimary: "#f564a9",
+              colorBorder: "#f564a9",
+              colorPrimaryActive: "#f564a9",
+              colorText: "#533b4d",
+              colorLinkActive: "#fae3c6",
+              colorPrimaryHover: "#f564a9",
+            },
+            Modal: {
+              colorBgMask: "rgba(245, 100, 169, 0.15)",
+            },
+          },
+        }}
+      >
+        <Button onClick={showModal}>+ New Activity</Button>
+        <Modal
+          className="modalLib"
+          footer={null}
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <div className="ticket">
+            <div className="ticket-time">
+              {selectedInput === "appointment" && <div>HI</div>}
 
-      },
-    }}
-  >
-      <Button onClick={showModal}>
-        + New Activity
-      </Button>
-      <Modal className='modalLib' footer={null} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-      <div className="ticket">
-        <div className='ticket-time'>
-          {selectedInput === 'appointment' && (
-              <div>HI</div>
-          )}
+              {selectedInput === "habit" && <div>Byee</div>}
+              <div>
+                <label>
+                  <input
+                    type="checkbox"
+                    value="appointment"
+                    checked={selectedInput === "appointment"}
+                    onChange={handleCheckboxChange}
+                  />
+                  Appointment
+                </label>
+                <label>
+                  <input
+                    type="checkbox"
+                    value="habit"
+                    checked={selectedInput === "habit"}
+                    onChange={handleCheckboxChange}
+                  />
+                  Habit
+                </label>
+              </div>
+            </div>
+            <div className="ticket-info">
+              <Form
+                form={form}
+                name="basic"
+                labelCol={{ span: 7 }}
+                wrapperCol={{ span: 17 }}
+                style={{ maxWidth: 700, padding: 5 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+              >
+                <Form.Item
+                  name="event"
+                  label="Title"
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
 
-          {selectedInput === 'habit' && (
-              <div>Byee</div>
-          )}
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              value="appointment"
-              checked={selectedInput === 'appointment'}
-              onChange={handleCheckboxChange}
-            />
-            Appointment
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              value="habit"
-              checked={selectedInput === 'habit'}
-              onChange={handleCheckboxChange}
-            />
-            Habit
-          </label>
-      </div>
+                <Form.Item
+                  name="description"
+                  label="Description"
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
 
-        </div>
-        <div className='ticket-info'>
-          <Form
-            form={form}
-            name="basic"
-            labelCol={{ span: 7 }}
-            wrapperCol={{ span: 17 }}
-            style={{ maxWidth: 700, padding: 5 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-          <Form.Item name="event" label="Title" rules={[{ required: true }]}>
-            <Input />
-          </Form.Item>
+                {selectedInput === "appointment" && (
+                  <Form.Item name="daterange" label="Date Range">
+                    <RangePicker
+                      cellRender={(current, info) => {
+                        if (info.type !== "date") return info.originNode;
+                        const style: React.CSSProperties = {};
+                        if (current.date() === 1) {
+                          style.border = "1px solid #1677ff";
+                          style.borderRadius = "50%";
+                        }
+                        return (
+                          <div className="ant-picker-cell-inner" style={style}>
+                            {current.date()}
+                          </div>
+                        );
+                      }}
+                    />
+                  </Form.Item>
+                )}
+                <Form.Item name="hourrange" label="Time Range">
+                  <TimePicker.RangePicker
+                    minuteStep={30}
+                    format={format}
+                    disabledTime={disabledDateTime}
+                    hourStep={1}
+                  />
+                </Form.Item>
 
-          <Form.Item name="description" label="Description"  rules={[{ required: true }]}>
-            <Input
-            />
-          </Form.Item>
-          
-          {selectedInput === 'appointment' && (
-              <Form.Item name="daterange" label="Date Range" >
-              <RangePicker cellRender={(current, info) => {
-                  if (info.type !== 'date') return info.originNode;
-                  const style: React.CSSProperties = {};
-                  if (current.date() === 1) {
-                    style.border = '1px solid #1677ff';
-                    style.borderRadius = '50%';
-                  }
-                  return (
-                    <div className="ant-picker-cell-inner" style={style}>
-                      {current.date()} 
-              </div>);}}/>
-              </Form.Item>
-          )}
-          <Form.Item name="hourrange" label="Time Range">
-          <TimePicker.RangePicker minuteStep={30}  format={format} disabledTime={disabledDateTime}  hourStep={1} />
-            </Form.Item>
-                  
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit" >
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-
-        </div>
-      </div>
-      </Modal>
-    </ConfigProvider>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          </div>
+        </Modal>
+      </ConfigProvider>
     </>
-
   );
 };
 

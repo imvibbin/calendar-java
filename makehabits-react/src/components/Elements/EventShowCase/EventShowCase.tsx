beb6aea9c60ad6f8ed3ef5880224410c65ea70e9
John Kevin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-08-29 22:33:52 +0200

    En la lista de eventos se pueden visualizar y eliminar los elementos

--- makehabits-react/src/components/Elements/EventShowCase/EventShowCase.tsx
+++ makehabits-react/src/components/Elements/EventShowCase/EventShowCase.tsx
@@ -1,101 +1,111 @@
 /* import React from "react"; */
-import gym from "../../../assets/images/FrontPage/ActivitiesShowCase/gym.png";
-import work from "../../../assets/images/FrontPage/ActivitiesShowCase/work.png";
 import study from "../../../assets/images/FrontPage/ActivitiesShowCase/study.png";
-import cook from "../../../assets/images/FrontPage/ActivitiesShowCase/cook.png";
-import cleaning from "../../../assets/images/FrontPage/ActivitiesShowCase/cleaning.png";
 import user from "../../../assets/logo-user.png";
-import './EventShowCase.scss'
+import UserInterface from "../../../models/UserInterface";
+import { deleteActivity } from "../../../services/ActivityService";
+import "./EventShowCase.scss";
+import { useState } from "react";
+import CustomError from "../../../models/CustomError";
+import { ToastContainer, toast } from "react-toastify";
 
+const notification = (deleteSuccess: boolean) => {
+  const toastMessage = deleteSuccess ? "Success!" : "Cannot be deleted";
 
+  toast[deleteSuccess ? "success" : "error"](toastMessage, {
+    position: "top-center",
+    autoClose: deleteSuccess ? 1000 : 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
 
-const durationActivity = '1:30';
-function EventShowCase() {
-  const eventsWithImages = [
-    {
-      name: "Cleaning",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: cleaning,
-    },
-    {
-      name: "Gym",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: gym,
-    },
-    {
-      name: "Work",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: work,
-    },
-    {
-      name: "Study",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: study,
-    },
-    {
-      name: "Work",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: work,
-    },
-    {
-      name: "Cooking",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: cook,
-    },
-    {
-      name: "Gym",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: gym,
-    },
-    {
-      name: "Work",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: work,
-    },
-    {
-      name: "Study",
-      timeLapse: `TimeLapse ${durationActivity}`,
-      imageUser: user,
-      imageUrl: study,
-    },
+const EventShowCase = () => {
+  const userData: UserInterface =
+    JSON.parse(localStorage.getItem("USER_DATA") ?? "{}") || null;
 
-  ];
+  const [eventsData, setEventsData] = useState(
+    Array.isArray(userData.activities)
+      ? userData.activities.map((event) => {
+          const activity = userData.activities[0];
+          const [startHourStr, endHourStr] =
+            activity.task_hour_range.split("|");
+          return {
+            id: event.task_id,
+            name: event.task_name,
+            timeLapse: `${startHourStr} - ${endHourStr}`,
+            imageUser: user,
+            imageUrl: study,
+          };
+        })
+      : [],
+  );
+
+  const handleOnClick = async (eventId: number) => {
+    try {
+      await deleteActivity(eventId);
+      notification(true);
+
+      // Update the eventsData state variable
+      setEventsData((prevState) => {
+        const updatedEvents = prevState.filter((event) => event.id !== eventId);
+        return updatedEvents;
+      });
+
+      // Update the USER_DATA item in the local storage
+      const updatedUserData = { ...userData };
+      updatedUserData.activities = updatedUserData.activities.filter(
+        (event) => event.task_id !== eventId,
+      );
+      localStorage.setItem("USER_DATA", JSON.stringify(updatedUserData));
+    } catch (error) {
+      const backendError = error as CustomError; // Cast to custom error type
+      if (backendError.message) {
+        notification(false);
+        console.error("Failed to authenticate user:", backendError.message);
+      } else {
+        console.error(
+          "Failed to authenticate user: An unknown error occurred.",
+        );
+      }
+    }
+  };
 
   return (
     <div className="showcase-habit overflow-y-scroll  ">
-      {eventsWithImages.map((event, index) => (
+      {eventsData.map((event, index) => (
         <div key={index} className="habit">
           <div className="habit__image">
-            <img src={event.imageUrl}/>
+            <img src={event.imageUrl} />
           </div>
           <div className="habit__info">
-            <div className="info__title">
-              {event.name}
-            </div>
+            <div className="info__title">{event.name}</div>
             <div className="info__image">
-              <img src={event.imageUser}/>
-            <span>Home</span>
-            {event.timeLapse}
-
+              <img src={event.imageUser} />
+              <span>Home</span>
+              {event.timeLapse}
             </div>
           </div>
-          <div className=" btn-custom-btn">
+          <div className="btn-custom-btn">
             <ul>
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 128 512" fill="#A5B4CB" > <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" /> </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 -960 960 960"
+                onClick={() => handleOnClick(event.id)}
+              >
+                <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+              </svg>
             </ul>
-          </div> 
+          </div>
         </div>
       ))}
+      <ToastContainer />
     </div>
   );
-}
+};
 
 export default EventShowCase;

8c55993ce05c61b1a0287a6eb90e383c7d3f4578
John Kevin Valle Cabuntasan <johnkvcode31@gmail.com>
2023-08-29 22:35:54 +0200

    le paso el array de dias al calendario (sin react-grid-layout) y muestro el calendario

--- makehabits-react/src/pages/CalendarPage/Calendar.tsx
+++ makehabits-react/src/pages/CalendarPage/Calendar.tsx
@@ -3,12 +3,16 @@ import WeekRender from "../../components/Calendar/WeekRender/WeekRender";
 import HourRender from "../../components/Calendar/HourRender/HourRender";
 import "./Calendar.css";
 
-const CalendarPage = () => {
+interface WeeklyViewProps {
+  weeklyViewData: string[]; // Replace 'any' with the appropriate type for your data
+}
+
+const CalendarPage: React.FC<WeeklyViewProps> = ({ weeklyViewData }) => {
   return (
     <>
       <div className="row week-container h-100">
-        <WeekRender />
-        <HourRender />
+        <WeekRender weeklyViewData={weeklyViewData} />
+        <HourRender weeklyViewData={weeklyViewData} />
       </div>
     </>
   );
--- makehabits-react/src/pages/Layout/Layout.tsx
+++ makehabits-react/src/pages/Layout/Layout.tsx
@@ -1,14 +1,15 @@
-import  { useState, /* useEffect */ } from 'react';
-import WeeklyView from '../../components/WeeklyView/WeeklyView';
-import MonthCalendarLib from '../../components/Lib/AntLib/MonthCalendarLib/MonthCalendarLib';
-import PopUpLib from '../../components/Lib/AntLib/PopUpLib/PopUpLib';
-import EventShowCase from '../../components/Elements/EventShowCase/EventShowCase';
-import NavBar from '../../components/Elements/NavBar/NavBar';
+import { useState /* useEffect */ } from "react";
+import WeeklyView from "../../components/WeeklyView/WeeklyView";
+import CalendarPage from "../CalendarPage/Calendar";
+import MonthCalendarLib from "../../components/Lib/AntLib/MonthCalendarLib/MonthCalendarLib";
+import PopUpLib from "../../components/Lib/AntLib/PopUpLib/PopUpLib";
+import EventShowCase from "../../components/Elements/EventShowCase/EventShowCase";
+import NavBar from "../../components/Elements/NavBar/NavBar";
 /* import type { Dayjs } from 'dayjs'; */
-import dayjs from 'dayjs'
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import dayjs from "dayjs";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 type StringArray = string[];
-import './Layout.scss/'
+import "./Layout.scss/";
 const Sidebar = ({ onCalendarWeekChange }) => {
   return (
     <aside className="sidebar">
@@ -24,22 +25,21 @@ const Sidebar = ({ onCalendarWeekChange }) => {
   );
 };
 
-
 const Layout = () => {
   const generateInitialWeeklyData = () => {
-    dayjs.extend(isSameOrBefore)
+    dayjs.extend(isSameOrBefore);
     const currentDate = dayjs();
-    const startOfWeek = currentDate.startOf('week');
-    const endOfWeek = currentDate.endOf('week');
-    
+    const startOfWeek = currentDate.startOf("week");
+    const endOfWeek = currentDate.endOf("week");
+
     const days = [];
     let currentDay = startOfWeek;
-    
-    while (dayjs(currentDay).isSameOrBefore(endOfWeek, 'day')) {
-      days.push(currentDay.format('YYYY-MM-DD'));
-      currentDay = currentDay.add(1, 'day');
+
+    while (dayjs(currentDay).isSameOrBefore(endOfWeek, "day")) {
+      days.push(currentDay.format("YYYY-MM-DD"));
+      currentDay = currentDay.add(1, "day");
     }
-    
+
     return days;
   };
   const handlePreviousWeek = () => {
@@ -58,23 +58,31 @@ const Layout = () => {
   };
 
   const initialWeeklyViewData = generateInitialWeeklyData();
-  const [weeklyViewData, setWeeklyViewData] = useState<StringArray>(initialWeeklyViewData);
-  const newData = ['1', '2'];
+  const [weeklyViewData, setWeeklyViewData] = useState<StringArray>(
+    initialWeeklyViewData,
+  );
+  const newData = ["1", "2"];
 
-
+  // return (
+  //   <div className="layout">
+  //     <NavBar previousWeek={handlePreviousWeek} nextWeek={handleNextWeek} />
+  //     <div className="content-wrapper">
+  //       <main className="main-content">
+  //         <WeeklyView weeklyViewData={weeklyViewData} />
+  //       </main>
+  //       <Sidebar onCalendarWeekChange={handleWeekSelection} />
+  //     </div>
+  //   </div>
+  // );
 
   return (
     <div className="layout">
-      <NavBar 
-        previousWeek={handlePreviousWeek}
-        nextWeek={handleNextWeek}
-      />
+      <NavBar previousWeek={handlePreviousWeek} nextWeek={handleNextWeek} />
       <div className="content-wrapper">
         <main className="main-content">
-          <WeeklyView weeklyViewData={weeklyViewData} />
+          <CalendarPage weeklyViewData={weeklyViewData} />
         </main>
-        <Sidebar onCalendarWeekChange={handleWeekSelection} 
-        />
+        <Sidebar onCalendarWeekChange={handleWeekSelection} />
       </div>
     </div>
   );
